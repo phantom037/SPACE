1c7309a24b7407e7bb6e5e3e62b5e154cb069459
Leo Tran <58055271+phantom037@users.noreply.github.com>
2022-08-11 21:05:22 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,140 +1,173 @@
-/* 
-Complete the following table when you submit this file:
-
-Surname     | Firstname | email | Contribution% | Any issues?
-=============================================================
-Eskandari   |  Arian    |  aesk0002@student.monash.edu     | 25%           |
-Nurmambetov. | Nail      |       | 25%           |
-Tran |     Thanh Dat      |   ttra0118@student.monash.edu    | 25%           |
-Person 4... |           |       | 25%           |
-
-complete Worksheet 1 by entering code in the places marked below...
-
-For full instructions and tests open the file worksheetChecklist.html
-in Chrome browser.  Keep it open side-by-side with your editor window.
-You will edit this file (main.js), save it, and reload the 
-browser window to run the test. 
-*/
+// Surname     | Firstname | Contribution % | Any issues?
+// =====================================================
+// Person 1... |           | 25%
+// Person 2... |           | 25%
+// Person 3... |           | 25%
+// Person 4... |           | 25%
+//
+// complete Worksheet 2 by entering code in the places marked below...
+//
+// For full instructions and tests open the file worksheetChecklist.html
+// in Chrome browser.  Keep it open side-by-side with your editor window.
+// You will edit this file (main.js), save it, and reload the
+// browser window to run the test.
 
 /**
-    Exercise 1:
-    The let and const keywords are for creating mutable and immutable variables 
-    respectively.
-    Create a mutable variable called ‘aVariable’ and assign its value to 1.
-*/
-// your first line of code here...
-let aVariable = 1;
+ * Exercise 1
+ */
+ let myObj = new Object();
+ myObj.aProperty = "Apple";
+ myObj.anotherProperty = 14;
+ 
+ //console.log(myObj);
+ 
+ /**
+  * Exercise 2
+  */
+ 
+  const operationOnTwoNumbers = function(f) {
+     return function(x) {
+         return function(y) {
+             return f(x,y);
+         };
+     };
+ };
+ 
+ //const add = operationOnTwoNumbers((x,y) => x + y)
+ //const addNine = add(9)(3)
+ //console.log(addNine)
+ 
+ /**
+  * Exercise 3
+  */
+ 
+ function callEach(array){
+     array.forEach(element => {
+         element();
+     });
+ }
+ 
+ //const fn1 = _ => console.log("Hey!");
+ //const fn2 = _ => console.log("Cool!");
+ //callEach([fn1, fn2]);
+ 
+ /**
+  * Exercise 4
+  */
+ function addN(n, array){
+    const add = operationOnTwoNumbers((x,y) => x + y);
+     let arr = [];
+     array.map(value => {
+         arr.push(add(value)(n));
+     });
+     return arr;
+ }
+ 
+ function getEvens(array){
+     let arr = [];
+     array.filter(value => {
+         if(value % 2 === 0){
+             arr.push(value);
+         }    
+     });
+     return arr;
+ }
 
-/*
-    Create an immutable variable called ‘aConst’ and assign its value to aVariable + 1.
-*/
-// your second line of code here...
-const aconst = aVariable + 1;
-
-/**
-    Exercise 2:
-    Create a function called ‘aFunction’ using the function keyword.  
-    Inside the function create another variable called 'anotherVariable',
-    set its value to 2 and return anotherVariable.
-*/
-function aFunction(){
-    let anotherVariable = 2;
-    return anotherVariable;
-}
-
-/**
-    Exercise 3:
-    Make a function called ‘projectEulerProblem1’ that calculates the answer using
-    mutable variables, a while loop, and returns the answer.
-*/
-function projectEulerProblem1(){
-    let result = 0, i = 1;
-    while(i < 1000){
-        if(i % 3 === 0 || i % 5 === 0){
-            result += i;
+ function multiplyArray(array){
+     let product = 1;
+     array.map(value =>{
+        if(value !== 0){
+            product *= value;
         }
-        i++;
-    }
-    return result;
+     });
+     return product;
+ }
+ //const result2 = multiplyArray(10);
+ //console.log(result2);
+ 
+ /**
+  * Exercise 5
+  */
+function range(n){
+    let i = n - 1;
+    if (i >= 0) {
+       return range(i).concat(i);
+    } else {
+       return [];
+  }
+}
+//console.log(range(5));
+ 
+ /**
+  * Exercise 6
+  */
+ function Euler1(){
+    return range(1000).filter(item => item % 3 === 0 || item % 5 === 0)
+    .reduce((result, item) => { 
+        return result + item;
+    }, 0);
+ }
+ const result3 = Euler1;
+ console.log(result3);
+ 
+ /**
+  * Exercise 7
+  */
+  function infinite_series_calculator(accumulate){
+        return function(predicate){
+            return function(transform){
+                return function(n){
+                    return range(n)
+                    .filter(x => predicate(x))
+                    .map(x => transform(x))
+                    .reduce((acc,x) => accumulate(acc, x))
+                }
+            }
+        }
 }
 
+ 
+ /**
+  * Exercise 8
+  */
+ function calculatePiTerm(n){
+    return(4*n**2)/(4*n**2 - 1);
+ }
 
-/**
-    Exercise 4:
-    Write a function called ‘alwaysTrue’ which always returns true, no matter what argument it is given.
-*/
-function alwaysTrue(x){
-    return true;
-}
+ function skipZero(n){
+    return n !== 0;
+ }
+
+ function productAccumulate(x,y){
+    return x * y;
+ }
+ 
+ function calculatePi(n){
     
-/**
-    Write a function called imperativeSummer that takes two parameters: a function f, and a number n.  
-    It should use an imperative loop to sum over the numbers from 1 up to (but not including) n,
-    including the number x in the sum only if f(x) is true.
-*/
-function imperativeSummer(f,n){
-    sum  = 0;
-    for (let i = 1; i < n; i++){
-        if (f(i)){
-            sum+=i;
-        }
-    }
-    return sum;
-
-}
-/**
-    Write a function called sumTo that takes as parameter a number n and
-    uses imperativeSummer and alwaysTrue to calculate the sum of all numbers
-    from 1 up to (but not including) n.
-*/
-function sumTo(n){
-    return imperativeSummer(alwaysTrue, n);
-}
-
-/**
-    Write a function called ‘isDivisibleByThreeOrFive’ which takes a number as parameter,
-    tests if it is divisible by 3 or 5, returning true if it is.
-*/
-function isDivisibleByThreeOrFive(n){
-    return (n % 3 === 0 || n % 5 === 0);
-}
-
-/**
-    Write a function called projectEulerProblem1UsingImperativeSummer 
-    that uses your imperativeSummer and isDivisibleByThreeOrFive to
-    again solve Project Euler Problem 1.  It should be one line of code!
-*/
-function projectEulerProblem1UsingImperativeSummer(n){
-    return imperativeSummer(isDivisibleByThreeOrFive, n);
-
-}
-
-/**
-    Exercise 5:
-    Write a function called 'immutableSummer' with parameters f and n, which computes the sum of numbers
-    from 1 up to (but not including) n that satisfy f, but does *not* use while,
-    for or any mutable variables (defined with let or var).
-    Hint: use recursion!
-*/
-function immutableSummer(f, n){
-    if((n - 1) === 0){
-        return 0;
-    }
-    if(f(n - 1)){
-        return n - 1 + immutableSummer(f, n - 1);
+ }
+ /**
+  * Exercise 9
+  */
+ function factorial(n){
+    if(n === 0){
+        return 1;
     }else{
-        return immutableSummer(f, n - 1);
+        return n * factorial(n - 1);
     }
-}
+ }
 
+ function calculateETerm(n){
+    return 2*(n + 1)/ factorial(2n + 1)
+ }
 
-/*
-    Write a function called projectEulerProblem1UsingImmutableSummer 
-    that uses your immutableSummer and isDivisibleByThreeOrFive to
-    again solve Project Euler Problem 1.  It should be one line of code!
-*/
-function projectEulerProblem1UsingImmutableSummer(n){
-    return immutableSummer(isDivisibleByThreeOrFive, n);
-}
+ function sumAccumulate(x, y){
+    return x + y;
+ }
 
-
+ function alwaysTrue(){
+    return true;
+ }
+ 
+ /**
+  * Exercise 10
+  */
